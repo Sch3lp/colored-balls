e6150c053ded5a2ae5a708d9244f7ff87872ec59
= <sch3lp@gmail.com>
2017-10-23 14:36:57 +0200

    fixed

--- src/main/java/Ball.java
+++ src/main/java/Ball.java
@@ -1,11 +1,25 @@
+import java.util.HashMap;
+
 public class Ball {
-    private static Ball ball;
+
+    private static HashMap<String, Ball> balls = new HashMap<>();
     private String color;
-    private Ball (String color) { this.color = color;}
-    public void bounce() { System.out.println("boing!"); }
-    public static Ball getInstance (String color) {
-        if (ball == null) { ball = new Ball(color); }
-        return ball;
+
+    private Ball(String color) {
+        this.color = color;
+    }
+
+    public void bounce() {
+        System.out.println("boing!");
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public static Ball getInstance(String color) {
+        balls.computeIfAbsent(color, Ball::new);
+        return balls.get(color);
     }
 }
 
--- src/test/java/BallTest.java
+++ src/test/java/BallTest.java
@@ -1,17 +1,49 @@
 import org.junit.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
 
 
 public class BallTest {
+
     @Test
     public void ballWithParameterBlue_shouldReturnBlueBall() throws Exception {
-
-
+        Ball blue = Ball.getInstance("blue");
+        assertThat(blue.getColor()).isEqualTo("blue");
     }
 
     @Test
     public void anotherBallWithParameterBlue_shouldReturnSameBlueBall() throws Exception {
+        Ball blueBall = Ball.getInstance("blue");
+        Ball secondBlueBall = Ball.getInstance("blue");
 
+        assertThat(blueBall).isSameAs(secondBlueBall); // blueBall == secondBlueBall
+        assertThat(blueBall.getColor()).isSameAs("blue");
+        assertThat(secondBlueBall.getColor()).isSameAs("blue");
+    }
 
+    @Test
+    public void anotherBallWithParameterRed_shouldReturnRedBall() throws Exception {
+        Ball blueBall = Ball.getInstance("blue");
+        Ball redBall = Ball.getInstance("red");
+        Ball secondRedBall = Ball.getInstance("red");
+
+        assertThat(redBall.getColor()).isEqualTo("red");
+        assertThat(blueBall).isNotSameAs(redBall); // blueBall != redBall
+        assertThat(secondRedBall).isSameAs(redBall); // secondRedBall == redBall
+    }
+
+    @Test
+    public void yetAnotherBallWithParameterYellow_shouldReturnYellowBall() throws Exception {
+        Ball blueBall = Ball.getInstance("blue");
+        Ball redBall = Ball.getInstance("red");
+        Ball yellowBall = Ball.getInstance("yellow");
+
+        assertThat(blueBall.getColor()).isEqualTo("blue");
+        assertThat(redBall.getColor()).isEqualTo("red");
+        assertThat(yellowBall.getColor()).isEqualTo("yellow");
+        
+        assertThat(blueBall).isNotSameAs(redBall);
+        assertThat(yellowBall).isNotSameAs(redBall);
+        assertThat(yellowBall).isNotSameAs(blueBall);
     }
 }
